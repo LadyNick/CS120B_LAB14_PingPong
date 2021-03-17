bf92ea8c812c345af4578271f6e37c01ccb7487e
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-03-17 04:36:08 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -8,16 +8,92 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <stdbool.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "scheduler.h"
+#include "timer.h"
 #endif
 
+void transmit_data(unsigned char data, unsigned char reg) {
+    //for some reason they values come out weird so you have to take each nibble, switch them and flip each nibble but not in the sense that you just flip 1 to 0 and 0 to 1, more like making abcd to dcba 
+    data = (data & 0xF0) >> 4 | (data & 0x0F) << 4; 
+    data = (data & 0xCC) >> 2 | (data & 0x33) << 2;
+    data = (data & 0xAA) >> 1 | (data & 0x55) << 1;
+	
+    int i;
+    if (reg == 1) {
+        for (i = 0; i < 8 ; ++i) {
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTC = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTC |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTC |= 0x02;  
+        }
+        // set RCLK = 1. 
+        PORTC |= 0x04;
+    }
+
+    else if (reg == 2) {
+        for (i = 0; i < 8 ; ++i) {
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTD = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTD |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTD |= 0x02;  
+        }
+        // set RCLK = 1. 
+        PORTD |= 0x04;
+    }
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRD = 0xFF; PORTD = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
 
-    /* Insert your solution below */
+
+    static task task1, task2, task3;
+    task *tasks[] = {&task1, &task2, &task3};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+    const char start = -1;
+	
+    //MOVE BALL + SPEED
+    task1.state = start;
+    task1.period = 300; //this is the base before all the ball physics
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &Ball_Tick;
+	
+    //MOVE PLAYER 1
+    task2.state = start;
+    task2.period = 300; //base speed for how fast the user can move their paddle 
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &Player1_Tick;
+	
+    //DISPLAY 
+    task3.state = start;
+    task3.period = 1; //constantly displaying
+    task3.elapsedTime = task2.period;
+    task3.TickFct = &Display_Tick;
+
+    TimerSet(1);
+    TimerOn();
+    
     while (1) {
-
+	    
+	    for(i=0; i<numTasks; i++){ //Scheduler code
+			if(tasks[i]->elapsedTime >= tasks[i]->period){
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
     }
     return 1;
 }
